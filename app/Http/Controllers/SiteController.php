a6d168e57d2262854d67ac9168edbf79f6f8fd09
AbdulRaheem sharaf <sharafkhalid531@gmail.com>
2023-07-10 14:36:15 +0300

    traning project

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    //
+    public function home() {
+return 'HOME PAGE';
+}
+
+public function team() {
+return 'TEAM PAGE';
+
+}
+
+public function group() {
+return 'GROUP PAGE';
+
+}
+}
---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Http\Controllers\SiteController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// home , about , contact , team , services
+
+// Route::get('home', function () {
+//     return 'home page';
+// });
+
+// Route::get('about', function () {
+//     return 'about page';
+// });
+
+// Route::get('contact', function () {
+//     return 'contact page';
+// });
+
+// Route::get('team', function () {
+//     return 'team page';
+// });
+
+Route::prefix('site')->group(function(){
+    Route::get('home',[SiteController::class,'home'])->name('home');
+    Route::get('team',[SiteController::class,'team'])->name('team');
+    Route::get('group',[SiteController::class,'group'])->name('group');
+});
